07857ef722cb0fa14e1a89a6abf27d8e38c4a070
sharontirza <92455645+sharontirza@users.noreply.github.com>
2022-09-14 11:14:44 +0700

    Delete spkmeans.h

--- spkmeans.h
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-double** make_mat(int n, int m);
-double** k_mean(int k, double **data_points, double **centroids, int vec_num,
-                int vec_size);                       
-double** main_by_goal(int k, int goal, double **matrix, int vec_num, int vec_size);
-void print_mat_rows(double **mat, int n, int m);
-void print_vec_row(double *vec, int n);
